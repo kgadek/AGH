8fd094e373f21cf6d7b4cbe33bdb544bea165c32
Konrad <kgadek@poczta.fm>
2010-03-29 21:59:27 +0200

    ASD::Sortowania(C): plik Makefile, podstawa sprawdzarki.

---
+++ Makefile
@@ -0,0 +1,4 @@
+CC 	= gcc
+CFLAGS	= -ansi -pedantic -Wall -W -Wconversion -Wshadow -Wcast-qual -Wwrite-strings -lmath
+LDFLAGS = -O2
+
---
+++ sort_test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "sorts.h"
+
+int main() {
+    int tab1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int tab2[] = {2, 1, 2, 2, 1,-1, 7, 7, 7, 7};
+    return 0;
+}
+
