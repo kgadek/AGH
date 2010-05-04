8466e4becda5070762c748032c28851f708a5632
Konrad Gądek <kgadek@poczta.fm>
2010-05-04 02:19:02 +0200

    Przepuszczenie skiplisty/trie by R.Lazarz przez indent, dopisanie Makefile'i

--- drzewa/rlazarz_trie.c
+++ drzewa/rlazarz_trie.c
@@ -1,126 +1,138 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define null 0
- 
-struct trienode{
- char font;
- struct trienode* other;
- struct trienode* next;
+
+struct trienode {
+    char font;
+    struct trienode *other;
+    struct trienode *next;
 };
- 
-void init(struct trienode** head){
- *head=null;
+
+void init(struct trienode **head)
+{
+    *head = NULL;
 }
- 
-struct trienode* insert(char* slowo, struct trienode* head){
-if(*slowo){
-    if(head){
-        struct trienode* p=head;
-        if(p->font <= *slowo){
-            while( p->other && (p->other->font <= *slowo) ) p=p->other;
-            if(p->font == *slowo){
-                slowo++;
-                 p->next=insert(slowo,p->next);
-                 }
-            else {
-                struct trienode* t=p->other;
-                p->other=(struct trienode*)malloc(sizeof(struct trienode));
-                p->other->other=t;
-                p->other->font=*slowo;
-                p->other->next=null;
-                slowo++;
-                p->other->next=insert(slowo,p->other->next);
-            }
-        }
-        else{
-        head=(struct trienode*)malloc(sizeof(struct trienode));
-        head->other=p;
-        head->font=*slowo;
-        head->next=null;
-        slowo++;
-        head->next=insert(slowo,head->next);
-        }   
+
+struct trienode *insert(const char *slowo, struct trienode *head) /* 'const char' by mozna bylo
+							 bezposrednio podac string */
+{
+    if (*slowo) {
+	if (head) {
+	    struct trienode *p = head;
+	    if (p->font <= *slowo) {
+		while (p->other && (p->other->font <= *slowo))
+		    p = p->other;
+		if (p->font == *slowo) {
+		    slowo++;
+		    p->next = insert(slowo, p->next);
+		} else {
+		    struct trienode *t = p->other;
+		    p->other =
+			(struct trienode *)
+			malloc(sizeof(struct trienode));
+		    p->other->other = t;
+		    p->other->font = *slowo;
+		    p->other->next = NULL;
+		    slowo++;
+		    p->other->next = insert(slowo, p->other->next);
+		}
+	    } else {
+		head = (struct trienode *) malloc(sizeof(struct trienode));
+		head->other = p;
+		head->font = *slowo;
+		head->next = NULL;
+		slowo++;
+		head->next = insert(slowo, head->next);
+	    }
+	} else {
+	    head = (struct trienode *) malloc(sizeof(struct trienode));
+	    head->other = NULL;
+	    head->font = *slowo;
+	    head->next = NULL;
+	    slowo++;
+	    head->next = insert(slowo, head->next);
+	}
     }
-    else {
-    head=(struct trienode*)malloc(sizeof(struct trienode));
-    head->other=null;
-    head->font=*slowo;
-    head->next=null;
-    slowo++;
-    head->next=insert(slowo,head->next);
-    }
+    return head;
 }
-return head;
-}
- 
-void wypisz(struct trienode* head, int deep){
-int i;
-while(head){
-    for(i=deep;i>0;i--) printf(" ");
-    printf("%c\n", head->font);
-    deep++;
-    wypisz(head->next,deep);
-    deep--;
-    head=head->other;
-}
-}
- 
-struct trienode* delete(char* slowo, struct trienode* head){
-if(*slowo){
-    struct trienode* p=head;
-    if(p->font == *slowo){
-        p->next=delete(++slowo,p->next);
-        if(!p->next){
-            head=p->other;
-            free(p);
-        }
-    }
-    else{
-        while(p->other->font != *slowo)p=p->other;
-        p->other->next=delete(++slowo,p->other->next);
-        if(!p->other->next){
-        struct trienode* t = p->other;
-        p->other=p->other->other;
-        free(t);    
-        }
+
+void wypisz(struct trienode *head, int deep)
+{
+    int i;
+    while (head) {
+	for (i = deep; i > 0; i--)
+	    printf(" ");
+	printf("%c\n", head->font);
+	deep++;
+	wypisz(head->next, deep);
+	deep--;
+	head = head->other;
     }
 }
-return head;
-}
- 
-int isnotin(char* slowo, struct trienode* head){
-int czy=0;
-if(*slowo){
-    while(head && (head->font != *slowo)) head=head->other;
-    if(!head) czy++;
-    else czy+=isnotin(++slowo,head->next);
+
+struct trienode *delete(const char *slowo, struct trienode *head) /* 'const char' by mozna bylo
+							 bezposrednio podac string */
+{
+    if (*slowo) {
+	struct trienode *p = head;
+	if (p->font == *slowo) {
+	    p->next = delete(++slowo, p->next);
+	    if (!p->next) {
+		head = p->other;
+		free(p);
+	    }
+	} else {
+	    while (p->other->font != *slowo)
+		p = p->other;
+	    p->other->next = delete(++slowo, p->other->next);
+	    if (!p->other->next) {
+		struct trienode *t = p->other;
+		p->other = p->other->other;
+		free(t);
+	    }
+	}
     }
-return czy;
+    return head;
 }
- 
- 
-int main(void){
-struct trienode* drzewo;
-init(&drzewo);
-drzewo=insert("radek",drzewo);
-drzewo=insert("rad",drzewo);
-drzewo=insert("radzio",drzewo);
-drzewo=insert("igor",drzewo);
-drzewo=insert("iga",drzewo);
-drzewo=insert("ignorancja",drzewo);
-drzewo=insert("iglica",drzewo);
-drzewo=insert("izydor",drzewo);
-drzewo=insert("absolutorium",drzewo);
-drzewo=insert("deizm",drzewo);
-drzewo=insert("radek",drzewo);
-drzewo=delete("radek",drzewo);
-wypisz(drzewo,0);
-int czy;
-czy=isnotin("rad",drzewo);
-printf("%i\n",czy);
-czy=isnotin("radzio",drzewo);
-printf("%i\n",czy);
-czy=isnotin("raca",drzewo);
-printf("%i\n",czy);
-return 0;
+
+int isnotin(const char *slowo, struct trienode *head) /* 'const char' by mozna bylo
+							 bezposrednio podac string */
+{
+    int czy = 0;
+    if (*slowo) {
+	while (head && (head->font != *slowo))
+	    head = head->other;
+	if (!head)
+	    czy++;
+	else
+	    czy += isnotin(++slowo, head->next);
+    }
+    return czy;
+}
+
+
+int main(void)
+{
+    struct trienode *drzewo;
+    int czy;
+    init(&drzewo);
+    drzewo = insert("radek", drzewo);
+    drzewo = insert("rad", drzewo);
+    drzewo = insert("radzio", drzewo);
+    drzewo = insert("igor", drzewo);
+    drzewo = insert("iga", drzewo);
+    drzewo = insert("ignorancja", drzewo);
+    drzewo = insert("iglica", drzewo);
+    drzewo = insert("izydor", drzewo);
+    drzewo = insert("absolutorium", drzewo);
+    drzewo = insert("deizm", drzewo);
+    drzewo = insert("radek", drzewo);
+    drzewo = delete("radek", drzewo);
+    wypisz(drzewo, 0);
+    czy = isnotin("rad", drzewo);
+    printf("%i\n", czy);
+    czy = isnotin("radzio", drzewo);
+    printf("%i\n", czy);
+    czy = isnotin("raca", drzewo);
+    printf("%i\n", czy);
+    return 0;
 }
--- listy/rlazarz_skiplist.c
+++ listy/rlazarz_skiplist.c
@@ -1,101 +1,117 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
- 
-const int max=5;
- 
-struct skipek{
-    struct skipek** nexts;
+
+const int max = 5;
+
+struct skipek {
+    struct skipek **nexts;
     int data;
 };
- 
-struct skipleb{
-    struct skipek* head;
+
+struct skipleb {
+    struct skipek *head;
     int wzrost;
 };
- 
-void init(struct skipleb* co){
-    co->wzrost=0;
-    co->head=(struct skipek*)malloc(sizeof(struct skipek));
-    co->head->nexts=(struct skipek**)calloc(max,sizeof(struct skipek*));
+
+void init(struct skipleb *co)
+{
     int i;
-    for(i=max;i>0;i--) co->head->nexts[i-1]=NULL;
+    co->wzrost = 0;
+    co->head = (struct skipek *) malloc(sizeof(struct skipek));
+    co->head->nexts =
+	(struct skipek **) calloc((size_t)max, sizeof(struct skipek *));
+    for (i = max; i > 0; i--)
+	co->head->nexts[i - 1] = NULL;
+}
+
+struct skipleb insert(struct skipleb gdzie, int co)
+{
+    struct skipek *temp = (struct skipek *) malloc(sizeof(struct skipek));
+    struct skipek *p;
+    int los = 1;
+    int i;
+    temp->data = co;
+    while ((rand() % 2) && los <= gdzie.wzrost)
+	los++;
+    if (los > max)
+	los = max;
+    if (los > gdzie.wzrost)
+	gdzie.wzrost = los;
+    temp->nexts = (struct skipek **) calloc((size_t)los, sizeof(struct skipek *));
+    p = gdzie.head;
+    for (i = gdzie.wzrost - 1; i >= 0; i--) {
+	while (p->nexts[i] && p->nexts[i]->data <= co)
+	    p = p->nexts[i];
+	if (i <= los - 1) {
+	    temp->nexts[i] = p->nexts[i];
+	    p->nexts[i] = temp;
+	}
     }
-    
-struct skipleb insert(struct skipleb gdzie, int co){
-    struct skipek* temp=(struct skipek*)malloc(sizeof(struct skipek));
-    temp->data=co;
-    int los=1;
-    while( (rand()%2) && los<=gdzie.wzrost ) los++;
-    if(los>max) los=max;
-    if(los>gdzie.wzrost) gdzie.wzrost=los;
-    temp->nexts=(struct skipek**)calloc(los,sizeof(struct skipek*));
-    int i;
-    struct skipek* p=gdzie.head;
-    for(i=gdzie.wzrost-1;i>=0;i--){
-        while(p->nexts[i] && p->nexts[i]->data<=co) p=p->nexts[i];
-        if(i<=los-1){
-            temp->nexts[i]=p->nexts[i];
-            p->nexts[i]=temp;
-            }
-        }
     return gdzie;
 }
- 
-struct skipleb del(int co, struct skipleb gdzie){
-int i;
-struct skipek* p=gdzie.head;
-for(i=gdzie.wzrost-1;i>0;i--){
-    while(p->nexts[i] && p->nexts[i]->data<co) p=p->nexts[i];
-    if(p->nexts[i] && p->nexts[i]->data==co ) p->nexts[i]=p->nexts[i]->nexts[i];
-    }
-while(p->nexts[0] && p->nexts[0]->data!=co) p=p->nexts[0];
-if(p->nexts[0]){
-    struct skipek* temp=p->nexts[0];
-    p->nexts[0]=p->nexts[0]->nexts[0];
-    free(temp);
-    }
-if(!gdzie.head->nexts[gdzie.wzrost-1]) gdzie.wzrost--;
-return gdzie;
-}
-    
-void wypisz(struct skipleb co){
+
+struct skipleb del(int co, struct skipleb gdzie)
+{
     int i;
-    struct skipek* p;
-    for(i=co.wzrost-1;i>=0;i--){
-        p=co.head;
-        while(p->nexts[i]){
-            printf("(%i)->",p->nexts[i]->data);
-            p=p->nexts[i];
-            }
-        printf("\n");
+    struct skipek *p = gdzie.head;
+    for (i = gdzie.wzrost - 1; i > 0; i--) {
+	while (p->nexts[i] && p->nexts[i]->data < co)
+	    p = p->nexts[i];
+	if (p->nexts[i] && p->nexts[i]->data == co)
+	    p->nexts[i] = p->nexts[i]->nexts[i];
+    }
+    while (p->nexts[0] && p->nexts[0]->data != co)
+	p = p->nexts[0];
+    if (p->nexts[0]) {
+	struct skipek *temp = p->nexts[0];
+	p->nexts[0] = p->nexts[0]->nexts[0];
+	free(temp);
+    }
+    if (!gdzie.head->nexts[gdzie.wzrost - 1])
+	gdzie.wzrost--;
+    return gdzie;
+}
+
+void wypisz(struct skipleb co)
+{
+    int i;
+    struct skipek *p;
+    for (i = co.wzrost - 1; i >= 0; i--) {
+	p = co.head;
+	while (p->nexts[i]) {
+	    printf("(%i)->", p->nexts[i]->data);
+	    p = p->nexts[i];
+	}
+	printf("\n");
     }
 }
-    
-int main(void){
-    srand(time(0));
+
+int main(void)
+{
     struct skipleb a;
+    srand((unsigned int)time(0));
     init(&a);
-    a=insert(a, 35);
-    a=insert(a, 5);
-    a=insert(a, 3);
-    a=insert(a, -35);
-    a=insert(a, 65);
-    a=insert(a, 15);
-    a=insert(a, 51);
-    a=insert(a, 31);
-    a=insert(a, 315);
-    a=insert(a, 6511);
-    a=insert(a, 0);
-    a=insert(a, 50);
-    a=insert(a, 30);
-    a=insert(a, -305);
-    a=insert(a, 605);
+    a = insert(a, 35);
+    a = insert(a, 5);
+    a = insert(a, 3);
+    a = insert(a, -35);
+    a = insert(a, 65);
+    a = insert(a, 15);
+    a = insert(a, 51);
+    a = insert(a, 31);
+    a = insert(a, 315);
+    a = insert(a, 6511);
+    a = insert(a, 0);
+    a = insert(a, 50);
+    a = insert(a, 30);
+    a = insert(a, -305);
+    a = insert(a, 605);
     wypisz(a);
     printf("\n");
-    a=del(50,a);
-    a=del(15,a);
-    a=del(66,a);
+    a = del(50, a);
+    a = del(15, a);
+    a = del(66, a);
     wypisz(a);
     return 0;
-    }
+}
